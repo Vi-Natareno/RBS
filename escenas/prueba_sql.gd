e688da2a3e41a21a06aab66d30fc96ac961c98be
Vi-Nat <157328098+Vi-Natareno@users.noreply.github.com>
2025-11-07 20:28:24 -0600

    Pantallas terminadas. Menu niveles y los juegos de platformas y ritmo no están completos

--- escenas/prueba_sql.gd
+++ escenas/prueba_sql.gd
@@ -11,3 +11,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_crear_tabla_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_insertar_datos_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_seleccionar_datos_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_actualizar_datos_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_eliminar_datos_pressed() -> void:
+	pass # Replace with function body.
